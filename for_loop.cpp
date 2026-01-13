662f9a879009f494a857a8406fb2a706df369e1f
karan7594 <karanprajapat723@gmail.com>
2026-01-13 09:49:05 +0530

    loops

---
+++ for_loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the number : ";
+    cin>>n;
+
+    for(int i=1;i<n;i++){
+        cout<<n<<"\n";
+    }
+    return 0;
+    
+}
